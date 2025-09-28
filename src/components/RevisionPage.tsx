abe52fa12eb5bf9bfd4c54dea1fc5c38382866cf
Lahori Venkatesh <lahorivenkatesh709@gmail.com>
2025-09-28 16:02:52 +0530

    Updated RevisionPage.tsx

--- src/components/RevisionPage.tsx
+++ src/components/RevisionPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BookOpen, CheckCircle, Circle, ExternalLink, StickyNote, Edit3, Trash2, Filter, Search } from 'lucide-react';
+import { BookOpen, CheckCircle, Circle, ExternalLink, StickyNote, CreditCard as Edit3, Trash2, Filter, Search } from 'lucide-react';
 import { useAppStore } from '../store';
 
 interface RevisionPageProps {
